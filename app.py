eb8bd621ab0e3eb91798e026457ff04258e71d72
Caner Turkmen <emoty88@gmail.com>
2023-07-12 16:43:18 -0700

    Get method handler added for service check

--- app.py
+++ app.py
@@ -2,12 +2,16 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+@app.route('/webhook', methods=['get'])
+def webhook():
+    if request.method == 'GET':
+        print("Data received from Webhook is: ", request.json)
+        return "Webhook Server Up Running!"
+    
 @app.route('/webhook', methods=['POST'])
 def webhook():
     if request.method == 'POST':
         print("Data received from Webhook is: ", request.json)
         return "Webhook received!"
-    else:
-        print('Webhook Server is up and running!')
 
 app.run(host='0.0.0.0', port=8088)
