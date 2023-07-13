0bb02c50c0ddb4eaeb0fde9e6f91b9317873eacd
Caner Turkmen <emoty88@gmail.com>
2023-07-12 17:42:14 -0700

    env example added. web hook run script mechanism updated.

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
+import os
 from flask import Flask, request
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 
-@app.route('/webhook', methods=['get'])
-def webhook():
-    if request.method == 'GET':
-        print("Data received from Webhook is: ", request.json)
-        return "Webhook Server Up Running!"
-    
-@app.route('/webhook', methods=['POST'])
+@app.route('/webhook', methods=['GET','POST'])
 def webhook():
     if request.method == 'POST':
-        print("Data received from Webhook is: ", request.json)
+        if request.headers.get('X-Hub-Signature') == os.environ.get('WEBHOOK_SECRET'):
+            os.system(os.environ.get('WEBHOOK_SCRIPT'))
         return "Webhook received!"
+    elif request.method == 'GET':
+        return "Webhook Server is up and running!"
 
 app.run(host='0.0.0.0', port=8088)
